870353eb5a642f8694c0615cc71a3436c7f9a703
Hurera Ranjha <hurera@swiftcontrol.systems>
2026-04-27 10:56:13 -0400

    Created Transactions file and constructor

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
-public class Transaction {
+import java.time.LocalDateTime;
+
+public class Transaction
+{
+    private LocalDateTime date;
+    private LocalDateTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(LocalDateTime date, LocalDateTime time, String description, String vendor, double amount)
+    {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
 }

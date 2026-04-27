d4fb1bb9f083b7f99b7624ca1848b538a50981d5
Hurera Ranjha <hurera@swiftcontrol.systems>
2026-04-27 11:31:11 -0400

    LoadHomeScreen method and switch statement for the different options

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,46 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main
 {
+    static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args)
     {
+        loadHomeScreen();
+    }
+
+    private static void loadHomeScreen()
+    {
+        boolean repeat = true;
+        while(repeat)
+        {
+            System.out.println("""
+                    Choose an action:
+                    (D) Deposit
+                    (P) Payment
+                    (L) Ledger
+                    (X) Exit
+                    """);
+            String selection = myScanner.nextLine();
+            switch (selection)
+            {
+                case "D":
+                    System.out.println("Deposit");
+                    break;
+                case "P":
+                    System.out.println("Payment");
+                    break;
+                case "L":
+                    System.out.println("Ledger");
+                    break;
+                case "X":
+                    System.out.println("Exit");
+                    repeat = false;
+                    break;
+            }
+        }
 
     }
 }

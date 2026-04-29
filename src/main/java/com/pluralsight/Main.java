f98eb3e1ffc096c0f92d2fb93749e814a9fbaf86
HureraRanjha <hureraranjha9@gmail.com>
2026-04-29 15:29:26 -0400

    Added custom filters, by dates, descripion, vendor, amount

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
@@ -179,6 +180,7 @@ public class Main
                     (3) Year to Date
                     (4) Previous Year
                     (5) Search by Vendor
+                    (6) Custom Search
                     (0) Back to ledger
                     """);
             // Month to date start of the month
@@ -203,6 +205,13 @@ public class Main
                     break;
                 case "5":
                     searchByVendor();
+                    break;
+                case "6":
+                    ArrayList<Transaction> filteredTransactions = applyCustomFilters(transactions);
+                    for (Transaction t: filteredTransactions)
+                    {
+                        System.out.println(t.displayTransaction());
+                    }
                     break;
                 case "0":
                     repeat = false;
@@ -269,13 +278,6 @@ public class Main
         }
     }
 
-//    public static ArrayList<Transaction> applyCustomFilters(ArrayList<Transaction> transactions) {
-//        ArrayList<Transaction> results = filterByDates(transactions);
-//        results = filterByDescription(results);
-//
-//        return  results;
-//    }
-
     private static void searchByVendor()
     {
         System.out.println("What vendor are you searching for? ");
@@ -287,5 +289,103 @@ public class Main
                 System.out.println(t.displayTransaction());
             }
         }
+    }
+
+    public static ArrayList<Transaction> applyCustomFilters(ArrayList<Transaction> transactions)
+    {
+        ArrayList<Transaction> results = filterByDates(transactions);
+        results = filterByDescription(results);
+        results = filterByVendor(results);
+        results = filterByAmount(results);
+
+        return  results;
+    }
+
+    private static ArrayList<Transaction> filterByDates(ArrayList<Transaction> transactions)
+    {
+        ArrayList<Transaction> transactionsByDate = new ArrayList<>();
+
+        System.out.println("What is the start date: ");
+        String startDate = myScanner.nextLine();
+
+        System.out.println("What is the end date: ");
+        String endDate = myScanner.nextLine();
+
+        LocalDate startSelection = null;
+        LocalDate endSelection = null;
+        if (!startDate.isEmpty()) {startSelection = LocalDate.parse(startDate);}
+        if(!endDate.isEmpty()) { endSelection = LocalDate.parse(endDate);}
+
+        for (Transaction t: transactions)
+        {
+            boolean afterStart = (startSelection == null || t.getDate().isAfter(startSelection));
+            boolean afterEnd = (endSelection == null || t.getDate().isBefore(endSelection));
+            if(afterStart && afterEnd)
+            {
+                transactionsByDate.add(t);
+                //System.out.println(t.displayTransaction());
+            }
+        }
+        return transactionsByDate;
+
+    }
+
+    private static ArrayList<Transaction> filterByDescription(ArrayList<Transaction> transactions)
+    {
+        ArrayList<Transaction> transactionsByDescription = new ArrayList<>();
+
+        System.out.println("What is the description: ");
+        String description = myScanner.nextLine();
+
+        for (Transaction t: transactions)
+        {
+            if(t.getDescription().contains(description))
+            {
+                transactionsByDescription.add(t);
+            }
+        }
+        return transactionsByDescription;
+    }
+
+    private static ArrayList<Transaction> filterByVendor(ArrayList<Transaction> transactions)
+    {
+        ArrayList<Transaction> transactionsByVendor = new ArrayList<>();
+
+        System.out.println("Who is the vendor: ");
+        String vendor = myScanner.nextLine();
+
+        for (Transaction t: transactions)
+        {
+            if(t.getVendor().contains(vendor))
+            {
+                transactionsByVendor.add(t);
+            }
+        }
+        return transactionsByVendor;
+    }
+
+    private static ArrayList<Transaction> filterByAmount(ArrayList<Transaction> transactions)
+    {
+        ArrayList<Transaction> transactionsByAmount = new ArrayList<>();
+
+        double minAmount = -Double.MAX_VALUE;
+        double maxAmount = Double.MAX_VALUE;
+
+        System.out.println("What is the minimum amount: ");
+        String minimumAmount = myScanner.nextLine();
+        if (!minimumAmount.isEmpty()) minAmount = Double.parseDouble(minimumAmount);
+
+        System.out.println("What is the maximum amount: ");
+        String maximumAmount = myScanner.nextLine();
+        if (!maximumAmount.isEmpty()) maxAmount = Double.parseDouble(maximumAmount);
+
+        for (Transaction t: transactions)
+        {
+            if(t.getAmount() >= minAmount && t.getAmount() <= maxAmount)
+            {
+                transactionsByAmount.add(t);
+            }
+        }
+        return transactionsByAmount;
     }
 }
